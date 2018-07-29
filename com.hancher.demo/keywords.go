8ec68e7845b5596cd95a5788e5d5ede1d4899b5c
hancher <hanchers@outlook.com>
2018-07-29 22:03:17 +0800

    记录go语言中的常用关键字

--- com.hancher.demo/keywords.go
+++ com.hancher.demo/keywords.go
@@ -1,5 +1,19 @@
 package main
 
 func main() {
-	
+	//25 个关键字
+	// break	default		func	interface	select
+	// case		defer		go 		map			struct
+	//chan		else		goto	package		switch
+	//const		if			range	type		fallthrough
+	//continue	for			import	return		var
+
+	//常量、类型、函数
+	//常量：true	false	iota 	nil
+	//类型：int			int8		int16		int32		int64
+	//		uint		uint8		uint16		uint32		uint64
+	//		float32		float64		complex64	complex128
+	//		bool		type		rung		string		error
+	//函数：make	len		cap		new		append		copy
+	// 		close	delete	complex	real	imag		panic	recover
 }
